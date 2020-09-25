07834804b3c4643d293e5856a2f0a861e28dbb8c
Luca Lumetti <lumetti.luca@gmail.com>
2020-09-25 12:39:31 +0200

    get songs of an artist

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ exports.getAlbumArt = require('./lib/getAlbumArt');
 exports.getSong = require('./lib/getSong');
 exports.searchSong = require('./lib/searchSong');
 exports.getSongById = require('./lib/getSongById');
+exports.getSongsByArtist = require('./lib/getSongsByArtist');
+exports._extractLyrics = require('./lib/utils/extractLyrics.js')
